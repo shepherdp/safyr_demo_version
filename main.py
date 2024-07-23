f38cf5a073db68aeeb83ca2d8f32de38320e82b2
shepherdp <88725439+shepherdp@users.noreply.github.com>
2024-07-23 00:02:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from shell import Shell
+
+def main():
+    s = Shell()
+
+if __name__ == '__main__':
+    main()
+
+
+# to get test coverage, type the following command in a terminal below:
+# pytest --cov-report=html:cov_html --cov=safyr safyr/test.py
